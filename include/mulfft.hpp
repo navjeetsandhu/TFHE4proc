d305bbe8808eeb80df6d648a67eb3ab776fdff24
navjeetsandhu <navjeet.sandhu@gmail.com>
2025-01-06 14:29:11 -0500

    changes

--- include/mulfft.hpp
+++ include/mulfft.hpp
@@ -29,7 +29,7 @@ inline void TwistIFFTbatch(PolynomialInFDn<P, batch> &res, const Polynomialn<P, 
 template <class P>
 inline void TwistFFT(Polynomial<P> &res, const PolynomialInFD<P> &a)
 {
-    //std::cout << "*";
+    std::cout << "*";
     if constexpr (std::is_same_v<P, lvl1param>)
         TwistFpgaFFT<P::n>(res, a);
     else if constexpr (std::is_same_v<typename P::T, uint64_t>)
@@ -54,7 +54,7 @@ inline void TwistFFTrescale(Polynomial<P> &res, const PolynomialInFD<P> &a)
 template <class P>
 inline void TwistIFFT(PolynomialInFD<P> &res, const Polynomial<P> &a)
 {
-    //std::cout << "%";
+    std::cout << "%";
     if constexpr (std::is_same_v<P, lvl1param>)
         TwistFpgaIFFT<P::n>(res, a);
     else if constexpr (std::is_same_v<typename P::T, uint64_t>)
